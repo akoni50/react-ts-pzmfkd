71e19945c14a732347f63b32df354a3a597cc182
akoni50 <39291600+akoni50@users.noreply.github.com>
2023-04-26 19:25:10 +0530

    added tsconfig.json

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import './style.css';
+import DOMPurify from 'dompurify'
 
 export default function App() {
+  console.log('stuff happends');
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
