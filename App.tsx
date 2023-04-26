5bf5babadd7bb404e4f5f27d1d56a42012378eb0
akoni50 <39291600+akoni50@users.noreply.github.com>
2023-04-26 20:03:43 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import './style.css';
-import DOMPurify from 'dompurify'
+import DOMPurify from 'dompurify';
 
 export default function App() {
   console.log('stuff happends');
+
+  let customHTML = '<div onmouseover=alert(1)>iam</div>';
+
+  let santizeHTML = DOMPurify.sanitize(customHTML);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <React.Fragment>
+      <div>
+        <p>{customHTML}</p>
+      </div>
+      <div>
+        <p>{santizeHTML}</p>
+      </div>
+    </React.Fragment>
   );
 }
